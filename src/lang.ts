1b1d7fbb71a9c6620e23f8021550d9d38dc78616
sauryanshu55 <95371800+sauryanshu55@users.noreply.github.com>
2023-03-15 14:20:27 -0500

    Added Sexp Parser and Lexer

--- src/lang.ts
+++ src/lang.ts
@@ -9,7 +9,7 @@ export type If = { tag: 'if', e1: Exp; e2: Exp; e3: Exp }
 export type Var = { tag: 'var', value: string }
 export type Img={tag:'image', value:Ext}
 export type Dir={tag:'dir', value:Ext}
-export type Matrix = number[][] |number;
+export type Matrix = {tag:'matrix', value:number[][] |number};
 
 export const evar = (value: string): Var => ({ tag: 'var', value })
 export const num = (value: number): Num => ({ tag: 'num', value })
@@ -18,6 +18,8 @@ export const ife = (e1: Exp, e2: Exp, e3: Exp): If => ({tag: 'if',e1,e2,e3,})
 export const charseq=(str:Charseq)=>({tag:'charseq',str})
 export const img=(img:Img)=>({tag:'img',img})
 export const dir=(img:Img)=>({tag:'dir',img})
+export const matrix=(m:Matrix)=>({tag:'matrix', matrix})
+    
   
 // Directory datatype and Image datatypes are Ext Types, External DataTypes
 export type Ext=Dir|Img
