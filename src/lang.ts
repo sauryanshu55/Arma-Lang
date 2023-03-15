c8f7bd2cf089690e9de070b4ea15e4492a2f990d
sauryanshu55 <95371800+sauryanshu55@users.noreply.github.com>
2023-03-15 18:03:12 -0500

    Implemented Types

--- src/lang.ts
+++ src/lang.ts
@@ -5,6 +5,14 @@
 /**
  * Types
  */
+export type TyNum = { tag: 'num' }
+export type TyCharseq = { tag: 'charseq' }
+export type TyBool = { tag: 'bool' }
+export type TyImg = { tag: 'img' }
+export type TyDir = { tag: 'dir' }
+export type TyMat = { tag: 'mat' }
+
+export type Typ = TyNum | TyCharseq | TyBool | TyImg | TyDir | TyMat
 
 /**
  * Expressions
@@ -29,7 +37,7 @@ export const charseq = (str: Charseq) => ({ tag: 'charseq', str })
 export const img = (loc: String) => ({ tag: 'img', loc })
 export const dir = (loc: String) => ({ tag: 'dir', loc })
 export const mat = (m: Mat) => ({ tag: 'mat', m })
-export const lam = (param: string, typ: Typ, body: Exp): Lam => ({tag: 'lam',param,typ,body,})
+export const lam = (param: string, typ: Typ, body: Exp): Lam => ({ tag: 'lam', param, typ, body, })
 export const app = (head: Exp, args: Exp[]): App => ({ tag: 'app', head, args })
 
 export type Exp = Var | Num | Bool | Charseq | If | Dir | Img | Mat | Lam | App
@@ -38,7 +46,7 @@ export type Exp = Var | Num | Bool | Charseq | If | Dir | Img | Mat | Lam | App
  * Values
  */
 
-export type Value=Num | Charseq | Bool | Img | Dir | Mat
+export type Value = Num | Charseq | Bool | Img | Dir | Mat
 
 /**
  * Statements
@@ -49,11 +57,11 @@ export type SDefine = { tag: 'define'; id: string; exp: Exp }
 export type SAssign = { tag: 'assign'; loc: Exp; exp: Exp }
 export type SPrint = { tag: 'print'; exp: Exp }
 
-export const sdefine = (id: string, exp: Exp): SDefine => ({tag: 'define',id,exp,})
-export const sassign = (loc: Exp, exp: Exp): SAssign => ({tag: 'assign',loc,exp,})
+export const sdefine = (id: string, exp: Exp): SDefine => ({ tag: 'define', id, exp, })
+export const sassign = (loc: Exp, exp: Exp): SAssign => ({ tag: 'assign', loc, exp, })
 export const sprint = (exp: Exp): SPrint => ({ tag: 'print', exp })
 
 /**
  * Program
  */
-export type Prog=Stmt[]
+export type Prog = Stmt[]
