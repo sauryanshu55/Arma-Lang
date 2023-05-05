973c155f76ccc91d864f6c38fb8c7e13abc6d3e2
Sauryanshu Chhetri (Khanal) <95371800+sauryanshu55@users.noreply.github.com>
2023-05-05 11:26:42 -0500

    Update interpreter.ts

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -18,20 +18,26 @@ export function evaluate(env: L.Env, e: L.Exp): L.Value {
     case 'bool':
       return e
     case 'lam':
-      return L.closure(e.param, e.body, env)
+      return L.closure(e.params, e.body, env)
     case 'app': {
       const head = evaluate(env, e.head)
-      const args = e.args.map(arg => evaluate(env, arg))
+      const args = e.args.map((arg) => evaluate(env, arg))
       if (head.tag === 'closure') {
-        if (args.length !== 1) {
-          throw new Error(`Runtime error: closure expects 1 argument but ${args.length} were given`)
+        if (args.length !== head.params.length) {
+          throw new Error(
+            `Runtime error: expected ${head.params.length} arguments, but found ${args.length}`
+          )
         } else {
-          return evaluate(head.env.extend1(head.param, args[0]), head.body)
+          return evaluate(head.env.extend(head.params, args), head.body)
         }
       } else if (head.tag === 'prim') {
         return head.fn(args)
       } else {
-        throw new Error(`Runtime error: expected closure or primitive, but found '${L.prettyExp(head)}'`)
+        throw new Error(
+          `Runtime error: expected closure or primitive, but found '${L.prettyValue(
+            head
+          )}'`
+        )
       }
     }
     case 'if': {
@@ -39,32 +45,21 @@ export function evaluate(env: L.Env, e: L.Exp): L.Value {
       if (v.tag === 'bool') {
         return v.value ? evaluate(env, e.e2) : evaluate(env, e.e3)
       } else {
-        throw new Error(`Type error: 'if' expects a boolean in guard position but a ${v.tag} was given.`)
+        throw new Error(
+          `Type error: 'if' expects a boolean in guard position but a ${v.tag} was given.`
+        )
       }
     }
-    case 'img':
-      return L.img(e.loc)
-    case 'charseq':
-      return L.charseq(e.value)
-    case 'dir':
-      return L.dir(e.loc)
+    case 'null': {
+      return e
+    }
+    case 'keyword': {
+      return e
+    }
+    // case 'object': {
+    //   return e
+    // }
   }
-}
-
-/**
- * Converts a string representation of an array of numbers to an actual array of numbers.
- *
- * @param data The input string representing an array of numbers.
- * @returns An array of numbers parsed from the input string.
- * @throws An error if the input string contains no numbers.
- */
-function convertParamToArray(data: string): number[] {
-  const regex = /\d+/g;
-  const matches = data.match(regex);
-  if (!matches) {
-    throw new Error("Input string contains no numbers");
-  }
-  return matches.map(Number);
 }
 
 /** @returns the result of executing program `prog` under environment `env` */
@@ -75,19 +70,6 @@ export function execute(env: L.Env, prog: L.Prog): Output {
       case 'define': {
         const v = evaluate(env, s.exp)
         env.set(s.id, v)
-        break
-      }
-      case 'assign': {
-        const rhs = evaluate(env, s.exp)
-        if (s.loc.tag === 'var') {
-          if (env.has(s.loc.value)) {
-            env.update(s.loc.value, rhs)
-          } else {
-            throw new Error(`Runtime error: unbound variable: ${s.loc.value}`)
-          }
-        } else {
-          throw new Error(`Runtime error: cannot assign to non-location '${L.prettyExp(s.loc)}'}`)
-        }
         break
       }
       case 'print': {
